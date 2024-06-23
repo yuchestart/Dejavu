7f18813ffaafeee3eb03e82ebe33945567fa661e
Che Yu <99137611+yuchestart@users.noreply.github.com>
2024-06-23 14:59:09 +0800

    Begin monster template for "Screaming Chaser"

---
+++ src/monsterbehaviors/screamingchaser.ts
@@ -0,0 +1,8 @@
+import { Entity } from "../gameplay/entities.js";
+import { Player } from "../gameplay/player.js";
+
+class ScreamingChaser extends Entity{
+    public behavior(player: Player){
+        
+    }
+}
